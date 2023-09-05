220bf95388ba0750f726382770863ea794e5e6ef
TMH <tmhkma18@gmail.com>
2023-09-05 10:30:53 +0700

    removing unused contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from './components'
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
 const App = () => {
-
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
+          <Navbar />
           <Hero />
         </div>
         <About />
         <Experience />
         <Tech />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-        </div>
+        {/* <Works /> */}
+        <div className="relative z-0"></div>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
